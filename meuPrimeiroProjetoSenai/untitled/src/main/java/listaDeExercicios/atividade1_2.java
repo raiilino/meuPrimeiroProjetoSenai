4a4e66714ae9499b8d2085a788ab3f7c2d4e9b84
38782067805 <raissaoliveiralino@gmail.com>
2026-09-03 14:49:58 -0300

    atividade1_2.java

---
+++ meuPrimeiroProjetoSenai/untitled/src/main/java/listaDeExercicios/atividade1_2.java
@@ -0,0 +1,10 @@
+// 1.2 Casting explícito
+class Main1_2 {
+    public static void main(String[] args) {
+        double varDouble = 56.45;
+        int varInt = (int) varDouble;
+
+        System.out.println("valor double: " + varDouble);
+        System.out.println("valor int: " + varInt);
+    }
+}
