a2a7ecd2f87278575f77ad885d0dc1bcaffa8939
38782067805 <raissaoliveiralino@gmail.com>
2026-09-03 14:42:54 -0300

    Atividade5

--- meuPrimeiroProjetoSenai/untitled/src/main/java/org/example/atividade5.java
+++ meuPrimeiroProjetoSenai/untitled/src/main/java/org/example/atividade5.java
@@ -10,28 +10,28 @@ public class atividade5 {
 
         switch (numero) {
             case 1:
-                System.out.println("Segunda");
+                System.out.println("Segunda ");
                 break;
             case 2:
-                System.out.println("Terça");
+                System.out.println("Terça ");
                 break;
             case 3:
-                System.out.println("Quarta");
+                System.out.println("Quarta ");
                 break;
             case 4:
-                System.out.println("Quinta");
+                System.out.println("Quinta ");
                 break;
             case 5:
-                System.out.println("Sexta");
+                System.out.println("Sexta ");
                 break;
             case 6:
-                System.out.println("Sábado");
+                System.out.println("Sábado ");
                 break;
             case 7:
-                System.out.println("Domingo");
+                System.out.println("Domingo ");
                 break;
             default:
-                System.out.println("Valor não reconhecido");
+                System.out.println("Valor não reconhecido ");
                 break;
         }
     }
